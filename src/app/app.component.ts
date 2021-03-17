66b39e9776efca8516f78b087f42b250a47fafd0
Lakshmi N Prasad Kakumani <kln550@users.noreply.github.com>
2021-03-17 09:44:15 -0400

    Angular Ag grid sample.
    Angular Ag grid sample.

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-ag-grid-app';
+  columnDefs = [
+    {headerName: 'Make', field: 'make'},
+    {headerName: 'Model', field: 'model'},
+    {headerName: 'Price', field: 'price'}
+  ];
+  rowData = [
+    {make: 'Toyota', model: 'Celica', price: 35000},
+    {make: 'Ford', model: 'Mondeo', price: 32000},
+    {make: 'Porsche', model: 'Boxter', price: 72000}
+  ];
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    fetch('https://api.myjson.com/bins/15psn9').
+      then(result => result.json()).
+      then(rowData => this.rowData = rowData);
+  }
+}
